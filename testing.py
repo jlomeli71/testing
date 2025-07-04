739ee0e354efc5315be9d14173ad1152c3427751
Jose M. Lomeli <jmlomeli@hotmail.com>
2025-07-03 20:48:21 -0600

    New code after refactoring

--- testing.py
+++ testing.py
@@ -1,9 +1,9 @@
-# Original code
 def process_transactions(transactions):
     total = 0
     for transaction in transactions:
-        if transaction['type'] == 'credit':
-            total += transaction['amount']
-        elif transaction['type'] == 'debit':
-            total -= transaction['amount']
+        amount = transaction.get('amount', 0)
+        if transaction.get('type') == 'credit':
+            total += amount
+        elif transaction.get('type') == 'debit':
+            total -= amount
     return total
